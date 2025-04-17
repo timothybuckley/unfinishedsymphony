826b468bf263298c71bc67ba6a368690421d6737
timothybuckley <tbuckley@altitude80.com>
2025-04-17 12:29:26 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default async function Home() {
   
   return (
     <main className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6 text-center">Cloudinary Gallery</h1>
+      <h1 className="text-3xl font-bold mb-6 text-center">Unfinished Symphony</h1>
       <Gallery images={images} />
     </main>
   );
